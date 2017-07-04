4b068213aa4ef5899307f73a58b162643b270128
Abraham Albert <abrahamalbert18@icloud.com>
2017-07-04 23:06:02 +0530

    Update All 4Digit Number Combinations.py

--- All 4Digit Number Combinations.py
+++ All 4Digit Number Combinations.py
@@ -1,15 +1,8 @@
-#ALL the combinations of 4 digit combo
+# ALL the combinations of 4 digit combo
 def FourDigitCombinations():
-    
+    numbers=[]
     for code in range(10000):
-        if code<=9:
-            print "000"+str(code),
-        elif code>=10 and code<=99:
-            print "00"+str(code),
-        elif code>=100 and code<=999:
-            print "0"+str(code),
-        else:
-            print code,
-            
-        
+        code=str(code).zfill(4)
+        print code,
+        numbers.append(code)
                 
