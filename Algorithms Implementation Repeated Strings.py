ab050228498640e2e29633039bac8b1ed6c68145
Sagar Gupta <sagargupta123kr@gmail.com>
2019-07-25 20:03:48 +0530

    Added for Python 3 in comments

--- Algorithms Implementation Repeated Strings.py
+++ Algorithms Implementation Repeated Strings.py
@@ -6,7 +6,10 @@ if number==0:
 elif number==1 and len(s)==1:
     print n
 else:
-     repeats=n/len(s)
+     #For Python2
+     repeats=n/len(s) 
+     #For Python3
+     #repeats=n//len(s) 
      remainders=n%len(s)
      #s=s*repeats + s[:remainders]
      print str(number*repeats+s[:remainders].count('a'))       
